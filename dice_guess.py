4324a2d257705d45ff53abddcef1f46f439128c6
Giacomo Lawrance <giacomolawrance@gmail.com>
2017-09-26 12:08:06 +0100

    added error handling
    If you do not enter a number, you get a error

--- dice_guess.py
+++ dice_guess.py
@@ -5,12 +5,15 @@ from random import randint
 result = (randint(1, 6))
 
 guess = input('Guess the number: ')
-if int(guess) == result:
-	print('You got it right!')
-else:
-	while int(guess) != result:
-		print('Wrong!\n')
-		guess = input('Guess the number: ')
-		if int(guess) == result:
-			print('You got it right!')
-			break
+try:
+	if int(guess) == result:
+		print('You got it right!')
+	else:
+		while int(guess) != result:
+			print('Wrong!\n')
+			guess = input('Guess the number: ')
+			if int(guess) == result:
+				print('You got it right!')
+				break
+except:
+	print('\nPlease enter a number.')

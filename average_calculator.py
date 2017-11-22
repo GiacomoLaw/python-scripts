bbb96955c48637ced6aafcdaa966d753f79274ed
Giacomo Lawrance <giacomolawrance@gmail.com>
2017-11-22 09:31:51 +0000

    Create average_calculator.py

---
+++ average_calculator.py
@@ -0,0 +1,23 @@
+test1 = []
+test2 = []
+test3 = []
+pupils = 0
+# put the number of students below
+students = 30
+
+while pupils <= students:
+    student_test1 = int(input("Test 1 result: "))
+    test1.append(student_test1)
+    student_test2 = int(input("Test 2 result: "))
+    test2.append(student_test2)
+    student_test3 = int(input("Test 3 result: "))
+    test3.append(student_test3)
+    pupils += 1
+
+total_mark = sum(test1) + sum(test2) + sum(test3)
+average1 = sum(test1) // students
+average2 = sum(test2) // students
+average3 = sum(test3) // students
+averageYear = total_mark // students
+
+print("Average for test 1 was", average1, "average for test 2 was", average2, "average for test 3 was", average3, "and the total average was", averageYear, ".")

36a3085d198601a8c775f1666d7acb4efe794b59
Giacomo Lawrance <giacomolawrance@gmail.com>
2017-11-22 09:32:56 +0000

    added better spacing

--- average_calculator.py
+++ average_calculator.py
@@ -6,11 +6,11 @@ pupils = 0
 students = 30
 
 while pupils <= students:
-    student_test1 = int(input("Test 1 result: "))
+    student_test1 = int(input("\n\nTest 1 result: "))
     test1.append(student_test1)
-    student_test2 = int(input("Test 2 result: "))
+    student_test2 = int(input("\nTest 2 result: "))
     test2.append(student_test2)
-    student_test3 = int(input("Test 3 result: "))
+    student_test3 = int(input("\nTest 3 result: "))
     test3.append(student_test3)
     pupils += 1
 
@@ -19,5 +19,6 @@ average1 = sum(test1) // students
 average2 = sum(test2) // students
 average3 = sum(test3) // students
 averageYear = total_mark // students
+
 
 print("Average for test 1 was", average1, "average for test 2 was", average2, "average for test 3 was", average3, "and the total average was", averageYear, ".")

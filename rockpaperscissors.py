fd88944f1d61b544152c290b45d8e04ff2a19d35
Giacomo Lawrance <giacomolawrance@gmail.com>
2017-11-29 10:40:48 +0000

    added repeating and wins/losses/drawing counters

--- rockpaperscissors.py
+++ rockpaperscissors.py
@@ -1,46 +1,55 @@
 from random import randint
-
-player = input("Rock, paper or scissors? ")
 wins = 0
 loses = 0
+turns = 0
+draws = 0
 
-computer = randint(1, 3)
-if computer == 1:
-	computer = 'rock'
-elif computer == 2:
-	computer = 'paper'
-else:
-	computer = 'scissors'
+while turns < 10:
+	player = input("Rock, paper or scissors? ")
+	turns += 1
 	
-print("You chose", player, "and the computer chose", computer)
-
-
-# computer = rock
-if computer == 'rock' and player == 'scissors':
-	print("You lose!")
-	loses += 1
-elif computer == 'rock' and player == 'paper':
-	print("You win!")
-	wins += 1
-elif computer == 'rock' and player == 'rock':
-	print("You draw!")
+	computer = randint(1, 3)
+	if computer == 1:
+		computer = 'rock'
+	elif computer == 2:
+		computer = 'paper'
+	else:
+		computer = 'scissors'
+		
+	print("You chose", player, "and the computer chose", computer)
 	
-# computer = paper
-elif computer == 'paper' and player == 'scissors':
-	print("You win!")
-	wins += 1
-elif computer == 'paper' and player == 'paper':
-	print("You draw!")
-elif computer == 'paper' and player == 'rock':
-	print("You lose!")
-	loses += 1
 	
-# computer = scissors
-elif computer == 'scissors' and player == 'scissors':
-	print("You draw!")
-elif computer == 'scissors' and player == 'paper':
-	print("You win!")
-	wins += 1
-elif computer == 'scissors' and player == 'rock':
-	print("You lose!")
-	loses += 1
+	# computer = rock
+	if computer == 'rock' and player == 'scissors':
+		print("You lose!")
+		loses += 1
+	elif computer == 'rock' and player == 'paper':
+		print("You win!")
+		wins += 1
+	elif computer == 'rock' and player == 'rock':
+		print("You draw!")
+		draws += 1
+		
+	# computer = paper
+	elif computer == 'paper' and player == 'scissors':
+		print("You win!")
+		wins += 1
+	elif computer == 'paper' and player == 'paper':
+		print("You draw!")
+		draws += 1
+	elif computer == 'paper' and player == 'rock':
+		print("You lose!")
+		loses += 1
+		
+	# computer = scissors
+	elif computer == 'scissors' and player == 'scissors':
+		print("You draw!")
+		draws += 1
+	elif computer == 'scissors' and player == 'paper':
+		print("You win!")
+		wins += 1
+	elif computer == 'scissors' and player == 'rock':
+		print("You lose!")
+		loses += 1
+		
+print('You won', wins, "times, and lost", loses, "times. You drew", draws, "times.")

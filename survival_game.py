a1dfca1026d85e6ec4ddc0b33d2f883ad2bbe119
Giacomo Lawrance <giacomolawrance@gmail.com>
2017-12-11 10:26:22 +0000

    fixed issues, working monster

--- survival_game.py
+++ survival_game.py
@@ -9,30 +9,30 @@ wood = 0
 stone = 0
 diamond = 0
 
-attack = 0
+attack = 1
 defense = 1
 health = 10
 
 turn = 0
 
-def monster_easy(food, turn):
-    chance = randint(0, 2)
-    if chance == 2:
-        m_health = 2
-        while m_health != 0:
-            print('You have been attacked!')
-            print('Monster has', m_health, 'health left.')
-            health = 9
-            m_health = m_health - attack
-            print("Monster now has", m_health, "health")
-            if health == 0:
-                print("You were killed! Try again.")
-                break
-        else m_health == 0:
-            print('You have defeated the monster!')
-            food += 1
-            turn += 1
-            print('You now have', food, "food, and", health, "health.")
-    return food
-
-monster_easy(food, turn)
+def monster_easy(health, food, turn):
+	chance = randint(0, 2)
+	if chance == 2:
+		m_health = 2
+		while m_health != 0:
+			print('You have been attacked!')
+			print('Monster has', m_health, 'health.')
+			health -= 1
+			m_health = m_health - attack
+			print("Monster now has", m_health, "health left, and you have", health, "left.")
+			if health == 0:
+				print("You were killed! Try again.")
+				break
+		if m_health == 0:
+			print('You have defeated the monster!')
+			food += 1
+			turn += 1
+			print('You now have', food, "food, and", health, "health.")
+	return food
+	
+monster_easy(health, food, turn)

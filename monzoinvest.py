1583aa41b2b43e6cf12c4386a5cf7f6c4def4e00
Giacomo Lawrance <giacomolawrance@gmail.com>
2018-12-04 15:27:31 +0000

    corrected credits

--- monzoinvest.py
+++ monzoinvest.py
@@ -1,4 +1,4 @@
-# based on https://github.com/GiacomoLaw/Python-scripts/blob/master/monzoinvest.py
+# update based on https://gist.github.com/lumisota/86663e0020825332075b67dd8d889f1f
 import requests
 
 url = "https://api.monzo.com/crowdfunding-investment/total"

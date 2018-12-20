19229eb8df7c2da6735562c22fca68f6ad9f417f
Giacomo Lawrance <giacomolawrance@gmail.com>
2018-12-20 15:11:28 +0000

    iss updates

---
+++ iss/iss.py
@@ -0,0 +1,39 @@
+import json
+import urllib.request
+import turtle
+
+# people in space
+url = 'http://api.open-notify.org/astros.json'
+response = urllib.request.urlopen(url)
+result = json.loads(response.read())
+
+print('People in Space: ', result['number'])
+
+people = result['people']
+
+for p in people:
+    print(p['name'])
+
+# location of iss
+url = 'http://api.open-notify.org/iss-now.json'
+response = urllib.request.urlopen(url)
+result = json.loads(response.read())
+
+location = result['iss_position']
+lat = location['latitude']
+lon = location['longitude']
+print('Latitude: ', lat)
+print('Longitude: ', lon)
+
+screen = turtle.Screen()
+screen.setup(720, 360)
+screen.setworldcoordinates(-180, -90, 180, 90)
+screen.bgpic('map.gif')
+
+screen.register_shape('iss.gif')
+iss = turtle.Turtle()
+iss.shape('iss.gif')
+iss.setheading(90)
+
+iss.penup()
+iss.goto(lon, lat)

91c366cf5ea7e8c0888f276e0ae4d5fdbd3b3a3c
Giacomo Lawrance <giacomolawrance@gmail.com>
2018-12-21 12:36:41 +0000

    fixed error message

--- scraper.py
+++ scraper.py
@@ -1,14 +1,14 @@
-import urllib
+import urllib.request
 from bs4 import BeautifulSoup
 
 url = input("What URL do you want to scrape? ")
 html = urllib.request.urlopen(url).read()
-soup = BeautifulSoup(html)
+soup = BeautifulSoup(html, features="html.parser")
 
 # remove all script and style elements
 for script in soup(["script", "style"]):
 	script.extract()
-	
+
 # get text
 text = soup.get_text()
 
